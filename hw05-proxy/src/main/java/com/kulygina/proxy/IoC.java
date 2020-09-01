3867a7cdee11107c30ee60eca0b54b67304bb008
medochek-coder <and.if.you.die18@gmail.com>
2020-09-01 13:09:16 +0300

    Created tests for IoC. Refactoring DemoInvocationHandler.

--- hw05-proxy/src/main/java/com/kulygina/proxy/IoC.java
+++ hw05-proxy/src/main/java/com/kulygina/proxy/IoC.java
@@ -25,35 +25,44 @@ public class IoC {
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            Method methodInImpl = getImplMethod(proxy, method);
+            Annotation[] declaredAnnotations = methodInImpl.getDeclaredAnnotations();
+            for (Annotation annotation : declaredAnnotations) {
+                if (annotation.annotationType().equals(Log.class)) {
+                    System.out.println("executed method: " + method.getName() + getArgsAsString(args));
+                }
+            }
+            return method.invoke(myClass, args);
+        }
+
+        private Method getImplMethod(Object proxy, Method method) throws Exception {
             DemoInvocationHandler handler = (DemoInvocationHandler) Proxy.getInvocationHandler(proxy);
             Class<? extends IClassForLogging> aClass = handler.myClass.getClass();
             Method[] allMethods = aClass.getDeclaredMethods();
             for (Method methodInImpl : allMethods) {
                 if (methodInImpl.getName().equals(method.getName())) {
-                    Annotation[] declaredAnnotations = methodInImpl.getDeclaredAnnotations();
-                    for (Annotation annotation : declaredAnnotations) {
-                        if (annotation.annotationType().equals(Log.class)) {
-                            if (args == null) {
-                                System.out.println("executed method: " + method.getName() + ", no param");
-                                continue;
-                            }
-                            String params = "";
-                            for (Object arg : args) {
-                                if (arg instanceof String[]) {
-                                    for (String s : (String[]) arg) {
-                                        params += s + ", ";
-                                    }
-                                } else {
-                                    params += arg.toString() + ", ";
-                                }
-                            }
-                            System.out.println("executed method: " + method.getName() + ", param: "
-                                    + params.substring(0, params.length() - 2));
-                        }
-                    }
+                    return methodInImpl;
                 }
             }
-            return method.invoke(myClass, args);
+            throw new Exception("Impossible exception! Method " + method.getName()
+                    + " does not exist in class " + aClass.getClass().getName());
+        }
+
+        private String getArgsAsString(Object[] args) {
+            String params = "";
+            if (args == null) {
+                return ", no param";
+            }
+            for (Object arg : args) {
+                if (arg instanceof String[]) {
+                    for (String s : (String[]) arg) {
+                        params += s + ", ";
+                    }
+                } else {
+                    params += arg.toString() + ", ";
+                }
+            }
+            return ", param: " + params.substring(0, params.length() - 2);
         }
 
         @Override
